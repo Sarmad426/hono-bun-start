d0df5dc0fccbf3d2f96760588de4d2d960d991fd
Sarmad rafique <sarmadrafique040@gmail.com>
2025-04-27 16:09:36 +0500

    Extended the Basic API file

--- api/basic-api.ts
+++ api/basic-api.ts
@@ -1,9 +1,25 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+
+import { env } from 'hono/adapter'
+import { ENV_TYPE } from "./type";
 
 const app = new Hono()
 
-app.get('/', (c) => {
-    return c.json({ message: 'Basic Hono API!' })
-})
+app.get("/", c => c.json({ "message": "Sarmad Practice API" }))
 
-export default app;
+app.get('/user', (c) => {
+
+    const { USER_NAME, USER_EMAIL, USER_ID } = env<ENV_TYPE>(c)
+    return c.json({
+        "id": USER_ID,
+        "name": USER_NAME,
+        "email": USER_EMAIL,
+        "message": "Hello from Hono API with bun!",
+    })
+}
+)
+
+export default {
+    fetch: app.fetch,
+    port: 8000,
+}
