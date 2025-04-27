d7652b56df0c53ad765e3fa86402f98a0cabd255
Sarmad rafique <sarmadrafique040@gmail.com>
2025-04-27 17:10:11 +0500

    Refactor database connection setup and user retrieval logic in index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,28 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import { env } from 'hono/adapter'
-import { ENV_TYPE } from './type'
-import { getUsers } from './db'
+// import { env } from 'hono/adapter'
+// import { ENV_TYPE } from './type'
+// import { getUsers } from './db'
+
+import { env } from 'bun'
+import { drizzle } from 'drizzle-orm/neon-http';
+import { user as userTable } from '../drizzle/schema';
+// import { Pool } from 'pg';
+import { neon } from '@neondatabase/serverless';
+
+const DATABASE_URL = env.DATABASE_URL
+
+// const pool = new Pool({
+//   connectionString: DATABASE_URL,
+// });
+
+// const db = drizzle(pool);
+
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle({ client: sql });
+
+export const getUsers = async () => await db.select().from(userTable).limit(10)
+
 
 const app = new Hono().basePath('/api')
 
@@ -10,16 +30,16 @@ app.get('/', (c) => {
   return c.json({ message: "Hono API from Sarmad Rafique!" })
 })
 
-app.get('/user', (c) => {
+// app.get('/user', (c) => {
 
-  const { USER_NAME, USER_EMAIL, USER_ID } = env<ENV_TYPE>(c)
-  return c.json({
-    "id": USER_ID,
-    "name": USER_NAME,
-    "email": USER_EMAIL,
-  })
-}
-)
+//   const { USER_NAME, USER_EMAIL, USER_ID } = env<ENV_TYPE>(c)
+//   return c.json({
+//     "id": USER_ID,
+//     "name": USER_NAME,
+//     "email": USER_EMAIL,
+//   })
+// }
+// )
 
 app.get("/users", async (c) => {
   const users = await getUsers();
