110fc2a56b47da2437fb7cbb91bf8067d8635db6
Sarmad rafique <sarmadrafique040@gmail.com>
2025-04-27 16:10:55 +0500

    Drizzle orm database connection configuration

---
+++ api/db.ts
@@ -0,0 +1,17 @@
+import { env } from 'bun'
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { user as userTable } from '../drizzle/schema';
+import { Pool } from 'pg';
+
+const DATABASE_URL = env.DATABASE_URL
+
+console.log("URL: ", DATABASE_URL)
+
+
+const pool = new Pool({
+    connectionString: DATABASE_URL,
+});
+
+const db = drizzle(pool);
+
+export const getUsers = async () => await db.select().from(userTable).limit(10)
