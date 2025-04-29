6b1a6ca32559cdbea7b53d4aa3734966552d762b
Sarmad rafique <sarmadrafique040@gmail.com>
2025-04-29 09:52:13 +0500

    Fix import statement for userTable and update DATABASE_URL assignment in db.ts

--- api/db.ts
+++ api/db.ts
@@ -1,9 +1,9 @@
-import { env } from 'bun'
+// import { env } from 'bun'
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { user as userTable } from '../drizzle/schema';
+import { userTable } from '../drizzle/schema';
 import { Pool } from 'pg';
 
-const DATABASE_URL = env.DATABASE_URL
+const DATABASE_URL = process.env.DATABASE_URL!
 
 const pool = new Pool({
     connectionString: DATABASE_URL,
