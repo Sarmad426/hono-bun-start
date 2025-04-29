ae46395fd688c722b2504f0d9b22f8c85bc01730
Sarmad rafique <sarmadrafique040@gmail.com>
2025-04-29 09:52:31 +0500

    Refactor index.ts by removing commented-out code and organizing imports

--- api/index.ts
+++ api/index.ts
@@ -1,34 +1,11 @@
-// import { env } from 'hono/adapter'
-// import { ENV_TYPE } from './type'
-// import { getUsers } from './db'
-// import { Pool } from 'pg';
-// const pool = new Pool({
-//   connectionString: DATABASE_URL,
-// });
-
-// const db = drizzle(pool);
-
-
-// app.get('/user', (c) => {
-
-//   const { USER_NAME, USER_EMAIL, USER_ID } = env<ENV_TYPE>(c)
-//   return c.json({
-//     "id": USER_ID,
-//     "name": USER_NAME,
-//     "email": USER_EMAIL,
-//   })
-// }
-// )
-
-
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
-import { drizzle } from 'drizzle-orm/neon-http'
-// import { user as userTable } from '../drizzle/schema'   // <- note .ts
-import { neon } from '@neondatabase/serverless'
-
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 import { pgTable, uniqueIndex, index, varchar, boolean } from "drizzle-orm/pg-core"
 
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle({ client: sql });
 
 
 const userTable = pgTable("userTable", {
@@ -44,23 +21,19 @@ const userTable = pgTable("userTable", {
 ]);
 
 
-const sql = neon(process.env.DATABASE_URL!)
-const db = drizzle({ client: sql })
+export const getUsers = async () => await db.select().from(userTable).limit(10);
 
-export const getUsers = async () =>
-  await db.select().from(userTable).limit(10)
+const app = new Hono().basePath("/api");
 
-const app = new Hono().basePath('/api')
+app.get("/", (c) => c.json({ message: "Hono API from Sarmad Rafique!" }));
+app.get("/users", async (c) => {
+  const users = await getUsers();
+  return c.json({ message: "Sarmad Practice API", users });
+});
 
-app.get('/', c => c.json({ message: 'Hono API from Sarmad Rafique!' }))
-app.get('/users', async c => {
-  const users = await getUsers()
-  return c.json({ message: 'Sarmad Practice API', users })
-})
-
-const handler = handle(app)
-export const GET = handler
-export const POST = handler
-export const PATCH = handler
-export const PUT = handler
-export const OPTIONS = handler
+const handler = handle(app);
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const PUT = handler;
+export const OPTIONS = handler;
